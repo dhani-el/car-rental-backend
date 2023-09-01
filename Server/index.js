8aa5a1aa8590adf6a9122adc6528ebba0f0f8cb7
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-01 12:25:40 +0100

    set app listen

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,13 @@
+require('dotenv').config()
 const Express  = require("express");
+const App = Express();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000
+
+App.get('/',function(req,res){
+    res.send("data is coming")
+})
+
+App.listen(port, function(){
+    console.log("server online at port:", port);
+})
