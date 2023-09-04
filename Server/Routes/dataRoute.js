d8a7c26534338409b40d0df3b090ca0c2e20b68a
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-04 11:31:42 +0100

    implement  s3 actions & attach  multer & s3 upload

--- Server/Routes/dataRoute.js
+++ Server/Routes/dataRoute.js
@@ -2,8 +2,7 @@ const express = require("express")
 const route = express.Router();
 const CAR_DB = require("../Schemas/CarsSchema");
 const BRAND_DB = require("../Schemas/BrandSchema");
-
-
+const {upload,randomName,fromS3,deleteFromS3, toS3} = require('../Utils/ImageUtils');
 
 
 route.get("/brands", async function(req,res){
@@ -31,8 +30,21 @@ route.get("/car/:id", async function(req,res){
     res.json(data);
 });
 
-route.post("/brand", async function(req,res){
-    const data = await BRAND_DB.create({name:req.body?.name ?? "N/A",
+route.post("/brand", upload.single("image"), async function(req,res){
+    const name  = randomName();
+    const body = req.body
+    const buffer = req.file.buffer
+    const mimetype = req.file.mimetype
+    if(!buffer){
+        console.log("no buffer");
+    return res.send("no buffer")
+}
+    console.log("this is the name",name);
+    console.log("this is the body",body);
+    console.log("this is the buffer",buffer);
+    console.log("this is the mimetype",mimetype);
+    await toS3(name,buffer, mimetype);
+    await BRAND_DB.create({name:name ?? "N/A",
                                         logo:req.body?.logo ?? "N/A"
     });
     res.send('data has been entered')
